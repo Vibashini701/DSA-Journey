b523d61cf49aef8cc27e01afa9db8e6b0baec79a
Vibashini S <vibashini701@gmail.com>
2026-06-22 14:35:35 +0000

    2pointer

---
+++  Leetcode_Twopointers/Sort_colors.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    void sortColors(vector<int>& nums) {
+        int left = 0;
+        int mid = 0;
+        int right = nums.size() - 1;
+        while(mid<=right){
+            if(nums[mid]==0){
+                swap(nums[left],nums[mid]);
+                mid++;
+                left++;
+            }
+            else if(nums[mid==2]){
+                swap(nums[right],nums[mid]);
+                right--;
+            }
+            else{
+                mid++;
+            }
+        }
+    }
+};
